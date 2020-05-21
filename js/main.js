2909fd5eca6d0a3ebba02780491f59e5c0d9a094
Vad11m-vek <55487186+Vad11m-vek@users.noreply.github.com>
2020-05-21 22:13:33 +0300

    get and save coockit for json

--- js/main.js
+++ js/main.js
@@ -1,57 +1,90 @@
 "use strict";
 
+//page one
+if (typeof (wraper_index) !== 'undefiend') {
 
-//index.html
-if (typeof (form001) !== 'undefined') {
-	console.log("index.html");
-	// мы на первойс странице?
-	//      кукиес нужные есть ? - ДА - редирект на вторую
-	//      нет кукиес - обрабатываем запорс от формы - сохраняем в куукиес - редирект на вторую
+	//listen page
+	document.addEventListener('click', event => {
+		let name = event.target.getAttribute('name');
+		if (name == 'start') check();
 
-	let myJson = {
-
-		email: 'info@novikov.ua',
-		password: 'qwerty'
+	});
 
 
+
+	//functions
+	function check() {
+		let $emailInput = document.querySelector('#email'),
+			$passwordInput = document.querySelector('#pass'),
+			myJson = {
+				email: '',
+				password: ''
+			};
+		myJson.email = $emailInput.value;
+		myJson.password = $passwordInput.value;
+
+		setCookie("myJson", JSON.stringify(myJson), 1);
+		let b = getCookieValue("myJson");
+		if (b !== "" && b.email !== "" && b.pass !== "") {
+			console.log('VadimMoLoDeTs')
+			document.location.href = "personal.html";
+		}
 	};
-	setCookie("myJson", JSON.stringify(myJson), 1)
 
 
 
 }
 
-//persoanl.html
-if (typeof (form002) !== 'undefined') {
-	console.log("persoanl.html");
-	exitclick.addEventListener("click", () => {
+
+
+//page too
+if (typeof (wrapper_personal) !== 'undefiend') {
+
+
+	let b = getCookieValue("myJson");
+	exit.addEventListener("click", () => {
 		//browser.cookies.remove();
 		setCookie("myJson", '', 1)
 		//eraseCookie("myJson");
 
+		if (b !== "") {
+			document.location.href = "index.html";
+		}
+
 	});
 
-	// мы на второй странице?
-	//      кукиес нужные есть ? - ДА - ждем данные формы
-	//      нет кукиес - редирект на первую
-	//      да куиес есть - обрабатываем запорс от формы - сохраняем в куукиес
-	//      Клик на EXIT - чистим кукиес - редикрет на перву.ю страницу
+	save.addEventListener('click', () => {
+		let $firstName = document.querySelector('#firstName'),
+			$secondName = document.querySelector('#secondName'),
+			$tel = document.querySelector('#tel'),
+			myJsonPageTwo = {
+				firstName: '',
+				secondName: '',
+				tel: '',
 
+			};
 
+		myJsonPageTwo.firstName = $firstName.value;
+		myJsonPageTwo.secondName = $secondName.value;
+		myJsonPageTwo.tel = $tel.value;
 
+		setCookie("myJsonPageTwo", JSON.stringify(myJsonPageTwo), 1);
 
+	});
 }
 
 
 
 
 
+// }
 
-
-
-
-
-
+function getCookieValue(name) {
+	let matches = document.cookie.match(new RegExp(
+		"(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+	));
+	return matches ? decodeURIComponent(matches[1]) : undefined;
+}
 
 function setCookie(name, value, days) {
 	var expires = "";
@@ -62,6 +95,7 @@ function setCookie(name, value, days) {
 	}
 	document.cookie = name + "=" + (value || "") + expires + "; path=/";
 }
+
 function getCookie(name) {
 	var nameEQ = name + "=";
 	var ca = document.cookie.split(';');
@@ -78,3 +112,39 @@ function eraseCookie(name) {
 
 	document.cookie = name + '=; Max-Age=-99999999;';
 }
+
+
+
+
+
+
+
+
+
+
+
+// function setCookie(name, value, days) {
+// 	var expires = "";
+// 	if (days) {
+// 		var date = new Date();
+// 		date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+// 		expires = "; expires=" + date.toUTCString();
+// 	}
+// 	document.cookie = name + "=" + (value || "") + expires + "; path=/";
+// }
+// function getCookie(name) {
+// 	var nameEQ = name + "=";
+// 	var ca = document.cookie.split(';');
+// 	for (var i = 0; i < ca.length; i++) {
+// 		var c = ca[i];
+// 		while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+// 		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+// 	}
+// 	return null;
+// }
+
+// function eraseCookie(name) {
+// 	console.log("eraseCookie for " + name);
+
+// 	document.cookie = name + '=; Max-Age=-99999999;';
+// }
